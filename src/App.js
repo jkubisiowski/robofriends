5617c76381e2f98a67fef6f27cb8a0f0f3dfc8ef
Jakub Kubisiowski <jakub.kubisiowski@fortedigital.no>
2019-06-23 12:37:27 +0200

    Add ErrorBoundry

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { robots } from './robots';
 import { SearchBox } from './components/SearchBox';
 import './App.css';
 import { Scroll } from './components/Scroll';
+import ErrorBoundry from './components/ErrorBoundry';
 
 class App extends Component {
   state = {
@@ -35,7 +36,9 @@ class App extends Component {
         <h1 className="f1">RoboFriends</h1>
         <SearchBox searchField={this.state.searchField} searchChanged={this.onSearchChanged} />
         <Scroll>
-          <CardList robots={this.state.robots} />
+          <ErrorBoundry>
+            <CardList robots={this.state.robots} />
+          </ErrorBoundry>
         </Scroll>
       </div>
     );
