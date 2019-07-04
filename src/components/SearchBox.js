f522b253b76bbbb7dd7b0b1f589c9fd36a0ed9da
Jakub Kubisiowski <jakub.kubisiowski@fortedigital.no>
2019-07-04 17:10:08 +0200

    Add accessiblity features

--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -4,6 +4,7 @@ export const SearchBox = ({ searchField, searchChanged }) => {
   return (
     <div className="pa2">
       <input
+        aria-label="Seach robots"
         value={searchField}
         onChange={searchChanged}
         className="pa3 ba b--green bg-lightest-blue"
