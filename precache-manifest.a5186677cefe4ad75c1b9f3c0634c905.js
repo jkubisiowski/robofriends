5c460753071593997363aba8fd82a4bacff8feff
Jakub Kubisiowski <jakub.kubisiowski@fortedigital.no>
2019-07-04 16:54:03 +0200

    Updates

---
+++ precache-manifest.a5186677cefe4ad75c1b9f3c0634c905.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "43347989b8c822f2b8fe993f5f188847",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "6523b8cf4b26399680c1",
+    "url": "/robofriends/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "660e16b0e6ba82900b1f",
+    "url": "/robofriends/static/css/main.3891b80a.chunk.css"
+  },
+  {
+    "revision": "6523b8cf4b26399680c1",
+    "url": "/robofriends/static/js/2.98507540.chunk.js"
+  },
+  {
+    "revision": "660e16b0e6ba82900b1f",
+    "url": "/robofriends/static/js/main.1af80800.chunk.js"
+  },
+  {
+    "revision": "143eb19aa518302daf32",
+    "url": "/robofriends/static/js/runtime~main.fb41d28d.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
