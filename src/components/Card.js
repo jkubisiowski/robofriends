c80411618580c71bc52d4baf579127c34f4369c0
Jakub Kubisiowski <jakub.kubisiowski@fortedigital.no>
2019-07-04 17:07:06 +0200

    Install gh-pages

--- src/components/Card.js
+++ src/components/Card.js
@@ -4,7 +4,7 @@ import 'tachyons';
 const Card = ({ id, name, email }) => {
   return (
     <div className="bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5 ">
-      <img src={`https://robohash.org/${id}?200x200`} alt="robot" />
+      <img src={`https://robohash.org/${id}?size=200x200`} alt="robot" />
       <div>
         <h2>{name}</h2>
         <p>{email}</p>
